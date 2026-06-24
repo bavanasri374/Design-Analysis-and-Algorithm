80f44aa32a484b7fb1f115e4b01f0d78ff6f3b2e
bavanasri374 <bavanasrig.cse2025@citchennai.net>
2026-06-24 14:40:42 +0530

    Add files via upload

---
+++ linear search.py
@@ -0,0 +1,15 @@
+def linear_search(arr, target):
+    for i in range(len(arr)):
+        if arr[i] == target:
+            return i
+    return -1
+
+arr = [12, 25, 7, 18, 30, 45, 22]
+target = 7
+
+result = linear_search(arr, target)
+
+if result != -1:
+    print("Element found at index", result)
+else:
+    print("Element not found")
